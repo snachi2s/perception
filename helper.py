73acb1576a19138152e8f5a3ffd75383896a61ef
Selva <91877888+snachi2s@users.noreply.github.com>
2025-02-19 23:51:30 +0100

    [add] script for converting grayscale masks to rgb

--- helper.py
+++ helper.py
@@ -1,6 +1,32 @@
 import os
 import shutil
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+KITTI_COLORMAP = {
+    0: (0, 0, 255),        
+    1: (128, 64, 128),    
+    2: (244, 35, 232),    
+    3: (70, 70, 70),      
+    4: (102, 102, 156),   
+    5: (190, 153, 153),   
+    6: (153, 153, 153),   
+    7: (250, 170, 30), 
+    8: (220, 220, 0),
+    9: (107, 142, 35),
+    10: (152, 251, 152),
+    11: (70, 130, 180),
+    12: (220, 20, 60),
+    13: (255, 0, 0),
+    14: (0, 0, 142),
+    15: (0, 0, 70),
+    16: (0, 60, 100),
+    17: (0, 80, 100),
+    18: (0, 0, 230),
+    19: (119, 11, 32)
+}
 
 def separate_stereo_images(source_folder, left_folder_name="left_cam", right_folder_name="right_cam"):
     """
@@ -8,8 +34,8 @@ def separate_stereo_images(source_folder, left_folder_name="left_cam", right_fol
 
     Args:
         source_folder (str): Path to the folder containing stereo images
-        left_folder_name (str): Name for the left image folder (default: 'left_cam')
-        right_folder_name (str): Name for the right image folder (default: 'right_cam')
+        left_folder_name (str): destination to store left cam images (default: 'left_cam')
+        right_folder_name (str): destination to store right cam images (default: 'right_cam')
     """
     left_folder = os.path.join(source_folder, left_folder_name)
     right_folder = os.path.join(source_folder, right_folder_name)
@@ -45,6 +71,45 @@ def extract_frames_from_video(input_video_path, output_folder='frames'):
     cap.release()
     #print(f"Extracted {frame_count} frames and saved in '{output_folder}'")
 
+def convert_grayscale_segmentation_masks_to_rgb(input_mask_path, out_path="rgb_masks"):
+    """Takes in grayscale masks (pixel values based on classes) and converts into RGB masks
+
+    Args:
+        input_mask_path: either one file or folder of images (if single image --> plot visualized)
+    """
+
+    if not os.path.exists(input_path): #sanity check
+        raise FileNotFoundError(f"Error: '{input_path}' does not exist.")
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    if os.path.isfile(input_path): 
+        paths, show = [input_path], True #single image --> visualization set to true
+    else:
+        paths = [
+            os.path.join(input_path, f) for f in os.listdir(input_path) 
+            if f.lower().endswith(('.png', '.jpg', '.jpeg', '.tif'))]
+        show = False
+
+    for p in paths:
+        mask = cv2.imread(p, cv2.IMREAD_UNCHANGED)
+
+        if mask is None: continue
+
+        #color mapping
+        rgb = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
+        for label, color in KITTI_COLORMAP.items():
+            rgb[mask == label] = color
+
+        out_file = os.path.join(out_path, f"rgb_{os.path.basename(p)}")
+        cv2.imwrite(out_file, cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
+
+        if show:
+            plt.imshow(rgb)
+            plt.axis('off')
+            plt.title("RGB Mask")
+            plt.show()
+
 
 
 def main():
@@ -59,6 +124,11 @@ def main():
     #========
     input_video_path = 'videoplayback.mp4'
     extract_frames_from_video(input_video_path)
+
+    #=========================
+    # grayscale masks --> RGB
+    #=========================
+    convert_grayscale_segmentation_masks_to_rgb('image.png')
 
 if __name__ == "__main__":
     main()
